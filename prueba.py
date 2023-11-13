c1e321cbc60e955a8865c559556a17f9aac4df81
axeloon <axelquezadac@gmail.com>
2023-11-13 02:39:41 -0300

    Se protegen las rutas mediante JWT
    Se limpia y comenta el código
    Se crean funciones para la codificación y decodificación del token JWT y una ruta para verificar el funcionamiento del JWT

--- prueba.py
+++ prueba.py
@@ -1,105 +1,134 @@
-from __future__ import print_function
-
+# Importa el módulo 'os.path' para realizar operaciones relacionadas con rutas y archivos en el sistema operativo
 import os.path
-import json
 
+# Importa las clases y módulos necesarios para la autenticación con Google oauth2
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-from fastapi import FastAPI
+# Importa las clases y módulos necesarios de FastAPI para la creación de la API
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 
-import jwt
+# Importa las funciones y la excepción necesarias de la librería 'jose' para trabajar con JSON Web Tokens (JWT)
+from jose import jwt, JWTError
 
+# Importa clases que son utilizadas para trabajar con fechas y duraciones en Python.
+from datetime import datetime, timedelta
 
-#Salto de linea (se puede optimizar)
-
-# If modifying these scopes, delete the file token.json.
+# Lista de ámbitos (scopes) para la autenticación con Google OAuth2
 SCOPES = ['https://www.googleapis.com/auth/contacts.readonly']
+PASSWD = '123456' #Contraseña para firmar el JWT
+ALGORITHM = "HS256" # Algoritmo utilizado para la codificación y decodificación del token JWT
 
+# Crea una instancia de la clase FastAPI para construir la aplicación web
 app = FastAPI()
+# Variable global para almacenar el token JWT, inicializada como None
+jwtToken = None
 
-passwd = '123456' #Contraseña para firmar el JWT
-
-br = f"""
-<br></br>
-"""
-
-@app.get("/", response_class=HTMLResponse)
-def root():
-    contenido_html = f""" 
+# Contenido HTML para una página web simple
+contenido_html = f""" 
         <h1>BOOKING UTEM</h1>
         <h2>Para ingresar, registrese con su cuenta utem</h2>
         <button onClick='fetch("http://127.0.0.1:5000/v1/login")'>
                 LOGIN
                 </button>
         <br></br>
+        <button onClick='fetch("http://localhost:5000/v1/protected")'>
+                Protected (Solo si tienes token)
+                </button>
+        <br></br>
         """
-    if os.path.exists('token.json') and os.path.exists('jwtToken.jwt'):
-        with open('token.json', 'r') as archivo:
-            token = json.load(archivo)
-        with open('jwtToken.jwt', 'r') as archivo:
-            jwt = archivo.read()
 
-        token = token["token"]
-        return(contenido_html + "Token: " + token + br + "JWT: " + jwt)
+# Función para verificar y decodificar un token JWT
+def verify_jwt():
+    global jwtToken     # Se llama a la variable global jwtToken
+    decode = jwtToken   # Se crea una copia de la variable global para utilizarla
+
+    # Excepción para manejar el caso en que el token no está disponible
+    credentialExeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token no disponible",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    # Excepción para manejar errores al decodificar el token
+    decodeExeption = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Error al decodificar el token",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    # Verifica si el token no está disponible y lanza la excepción correspondiente
+    if jwtToken is None:
+        raise credentialExeption
+    # Intenta decodificar el token y retorna el payload si tiene éxito
+    try:
+        payload = jwt.decode(decode, key=PASSWD, algorithms=[ALGORITHM])
+        return payload  # Retorna el payload del token JWT decodificado
+    # Lanza una excepción si hay un error al decodificar el token
+    except JWTError:
+        raise decodeExeption
     
-    else:
-        return (contenido_html)
 
+# Función para crear un token JWT a partir de las credenciales de Google
+def createJWT(google_token: Credentials):
 
-# algo así deberia ir
+    # Configura el payload del token JWT con información relevante
+    payload = {
+        "sub": google_token.token,
+        "exp": datetime.utcnow() + timedelta(minutes=15),
+        "scopes": SCOPES,
+        #Se puede ingresar más información si es necesario
+    }
+    # Codifica el token JWT usando la contraseña (PASSWD) y el algoritmo (ALGORITHM)
+    jwtoken = jwt.encode(payload, key=PASSWD, algorithm=ALGORITHM)
+    return jwtoken  # Retorna el token JWT recién creado
 
+    
+# Ruta principal de la aplicación que devuelve contenido HTML
+@app.get("/", response_class=HTMLResponse)
+def root():
+    # Verifica si existe el archivo 'token.json'
+    if os.path.exists('token.json'):
+        # Carga las credenciales desde el archivo 'token.json' y crea el JWT
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        createJWT(creds) #Crea el JWT con las credenciales de Google
+        return(contenido_html + "Token ya creado")
+    # Si no existe el archivo 'token.json', simplemente devuelve el contenido HTML
+    return (contenido_html)
 
+# Ruta para el proceso de inicio de sesión y generación del token JWT
 @app.get("/v1/login")
 def login():
-    HTMLResponse('''<h3> ERROR, TOKEN NO CREADO </h3>''')
+    global jwtToken
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    
+    # Verifica si existe el archivo 'token.json' y carga las credenciales
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+        jwtToken = createJWT(creds) #Crea el JWT con las credenciales de google
+    # Si no hay credenciales válidas, inicia el flujo de autorización
     if not creds or not creds.valid:
+        # Si las credenciales existen pero están vencidas, realiza una actualización
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
+            # Si no hay credenciales, inicia el flujo de autorización desde el archivo 'credentials.json'
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+          
+        # Guarda las credenciales en el archivo 'token.json'
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-    return "token creado"
+            jwtToken = createJWT(creds) #Crea el JWT con las credenciales de google
+    return jwtToken     # Retorna el token JWT generado  
 
 
-@app.get("/v1/token")
-def token():
-    if os.path.exists('token.json'):
-        with open('token.json', 'r') as archivo:
-            datos = json.load(archivo)
-    else:
-        return ('No existe token')
-    
-    return ("Token: " + datos["token"])
-
-@app.get("/v1/jwt")
-def jwtToken():
-    if os.path.exists('token.json'):
-        with open('token.json', 'r') as archivo:
-            datos = json.load(archivo)
-            #token = datos["token"]
-            jwtToken = jwt.encode(datos, passwd, algorithm='HS256')
-    with open('jwtToken.jwt', "w") as archivo:  #Guarda el JWT en la carpeta
-        archivo.write(jwtToken)
-
-    return ("JWT: " + jwtToken)
-
-
-if __name__ == '__main__':
-    main()
-        
+# Ruta protegida que requiere un token JWT para acceder
+@app.get("/v1/protected")   #Primero hay que pasar por el login, para que el token se genere, luego, al acceder a esta ruta se decodifica
+def protected_route(current_user: dict = Depends(verify_jwt)):  # Verifica y decodifica el token JWT enviado en la solicitud
+    # Retorna un diccionario con un mensaje y la información del usuario para la ruta protegida
+    return {
+        "message": "Esta es una ruta protegida",
+        "user": current_user["sub"]
+    }
